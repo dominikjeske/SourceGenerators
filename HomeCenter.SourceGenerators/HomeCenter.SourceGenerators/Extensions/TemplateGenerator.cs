2fe790fc7aa4a16609cc3de4d74774111802f959
Dominik Jeske <dominik.jeske@hotmail.com>
2020-11-08 16:23:43 +0100

    Guard template generation

--- HomeCenter.SourceGenerators/HomeCenter.SourceGenerators/Extensions/TemplateGenerator.cs
+++ HomeCenter.SourceGenerators/HomeCenter.SourceGenerators/Extensions/TemplateGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Scriban;
+using System;
+using System.Linq;
 
 namespace HomeCenter.SourceGenerators
 {
@@ -9,6 +11,13 @@ namespace HomeCenter.SourceGenerators
         public static string Execute(string templateString, object model)
         {
             var template = Template.Parse(templateString);
+
+            if(template.HasErrors)
+            {
+                var errors = string.Join(" | ", template.Messages.Select(x => x.Message));
+                throw new InvalidOperationException($"Template parse error: {template.Messages}");
+            }
+
             var result = template.Render(model, memberRenamer: member => member.Name);
 
             result = SyntaxFactory.ParseCompilationUnit(result)
